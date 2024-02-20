8e0f345ea353ef7980b8960e3b29ff18d0d17eed
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-02-20 02:43:43 -0800

    Second Commit

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,23 +9,23 @@
 
 void selection_sort(int *array, size_t size)
 {
-    int i, j, c, len, k;
+	int i, j, c, len, k;
 
-    len = size;
-    for (i = 0; i < len - 1; i++)
-    {
-        k = i;
-        for (j = i + 1; j < len; j++)
-        {
-            if (array[j] < array[k])
-                k = j;
-        }
-        if (k != i)
-        {
-            c = array[k];
-            array[k] = array[i];
-            array[i] = c;
-            print_array(array, size);
-        }
-    }
+	len = size;
+	for (i = 0; i < len - 1; i++)
+	{
+		k = i;
+		for (j = i + 1; j < len; j++)
+		{
+			if (array[j] < array[k])
+				k = j;
+		}
+		if (k != i)
+		{
+			c = array[k];
+			array[k] = array[i];
+			array[i] = c;
+			print_array(array, size);
+		}
+	}
 }
