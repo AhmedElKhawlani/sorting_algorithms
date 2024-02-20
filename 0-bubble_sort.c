51298be1921c43e2fcad29bc511180161765f690
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-02-20 01:50:13 -0800

    First Commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,20 +9,20 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    int i, j, c, len;
+	int i, j, c, len;
 
-    len = size;
-    for (i = 0; i < len; i++)
-    {
-        for (j = 0; j < len - i - 1; j++)
-            {
-                if (array[j] > array[j + 1])
-                    {
-                        c = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = c;
-                        print_array(array, size);
-                    }
-            }
-    }
+	len = size;
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < len - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				c = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = c;
+				print_array(array, size);
+			}
+		}
+	}
 }
