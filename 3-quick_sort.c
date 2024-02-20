78e623a159ef50051896276e88e72547d5b7f46d
AhmedElKhawlani <elkhawlani.python@gmail.com>
2024-02-20 03:29:22 -0800

    Third Commit

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,26 +11,26 @@
 
 int partition(int *array, int lo, int hi, size_t size)
 {
-    int pivot, i, j, c;
+	int pivot, i, j, c;
 
-    pivot = array[hi];
-    i = lo - 1;
-    for (j = lo; j < hi; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            c = array[i];
-            array[i] = array[j];
-            array[j] = c;
-        }
-    }
-    i++;
-    c = array[i];
-    array[i] = array[hi];
-    array[hi] = c;
-    print_array(array, size);
-    return (i);
+	pivot = array[hi];
+	i = lo - 1;
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			c = array[i];
+			array[i] = array[j];
+			array[j] = c;
+		}
+	}
+	i++;
+	c = array[i];
+	array[i] = array[hi];
+	array[hi] = c;
+	print_array(array, size);
+	return (i);
 }
 
 /**
@@ -43,13 +43,13 @@ int partition(int *array, int lo, int hi, size_t size)
 
 void lomuto(int *array, int lo, int hi, size_t size)
 {
-    int p;
+	int p;
 
-    if (lo >= hi || lo < 0)
-        return;
-    p = partition(array, lo, hi, size);
-    lomuto(array, lo, p - 1, size);
-    lomuto(array, p + 1, hi, size);
+	if (lo >= hi || lo < 0)
+		return;
+	p = partition(array, lo, hi, size);
+	lomuto(array, lo, p - 1, size);
+	lomuto(array, p + 1, hi, size);
 }
 
 /**
@@ -61,9 +61,9 @@ void lomuto(int *array, int lo, int hi, size_t size)
 
 void quick_sort(int *array, size_t size)
 {
-    int lo, hi;
+	int lo, hi;
 
-    lo = 0;
-    hi = size - 1;
-    lomuto(array, lo, hi, size);
+	lo = 0;
+	hi = size - 1;
+	lomuto(array, lo, hi, size);
 }
